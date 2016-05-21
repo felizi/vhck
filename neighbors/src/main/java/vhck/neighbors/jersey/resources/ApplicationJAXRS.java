d10063f341917057883657886e56cabf38921f08
Daniel Felizi <felizi.daniel@gmail.com>
2016-05-21 19:11:44 -0300

    Testes

---
+++ neighbors/src/main/java/vhck/neighbors/jersey/resources/ApplicationJAXRS.java
@@ -0,0 +1,18 @@
+/*package vhck.neighbors.jersey.resources;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+public class ApplicationJAXRS extends Application {
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> s = new HashSet<Class<?>>();
+		s.add(AuthResource.class);
+		s.add(UserResource.class);
+		return s;
+	}
+}
+*/
