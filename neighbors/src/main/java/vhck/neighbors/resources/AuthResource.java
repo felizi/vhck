c808eb1cf0b363cd0b96507ef5733a239a4036d8
Daniel Felizi <felizi.daniel@gmail.com>
2016-05-21 15:56:46 -0300

    Auth and User resource

---
+++ neighbors/src/main/java/vhck/neighbors/resources/AuthResource.java
@@ -0,0 +1,35 @@
+package vhck.neighbors.resources;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+@Path("auth")
+public class AuthResource {
+	/*@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response login(String usernamePasswordJson) {
+		Map<String, Object> usernamePasswordMap = new Gson().fromJson(usernamePasswordJson, new TypeToken<HashMap<String, Object>>() {
+		}.getType());
+		return Response.status(HttpServletResponse.SC_OK).entity(usernamePasswordMap).build();
+	}*/
+
+	@GET
+	@Path("/info/{token}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Response getUserByToken(@PathParam("token") Integer token) {
+		return Response.ok().entity("ok" + token).build();
+	}
+}
