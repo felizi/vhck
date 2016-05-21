0aa75a64a64529446257e8fd72ae547ab8a75903
Daniel Felizi <felizi.daniel@gmail.com>
2016-05-21 17:07:38 -0300

    Tests resource

--- neighbors/src/main/java/vhck/neighbors/resources/UserResource.java
+++ neighbors/src/main/java/vhck/neighbors/resources/UserResource.java
@@ -4,11 +4,18 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import vhck.neighbors.entity.UserEntity;
+import vhck.neighbors.entity.group.EventEntity;
+import vhck.neighbors.entity.message.MessageEntity;
+
 public class UserResource {
 	@GET
 	@Path("users")
 	@Produces("application/xml")
 	public String users() {
+		MessageEntity msg = new MessageEntity();
+		msg.setSender(new EventEntity());
+		msg.setRecipient(new UserEntity());
 		return "Hello World !! - Jersey 2";
 	}
 	
